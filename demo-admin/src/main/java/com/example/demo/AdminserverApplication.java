01373b80412e16eaadf943d9332271bce4d96952
bakito <github@bakito.ch>
2023-09-14 23:22:35 +0200

    discovery

--- demo-admin/src/main/java/com/example/demo/AdminserverApplication.java
+++ demo-admin/src/main/java/com/example/demo/AdminserverApplication.java
@@ -3,9 +3,13 @@ package com.example.demo;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAdminServer
+@EnableDiscoveryClient
+@EnableScheduling
 public class AdminserverApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminserverApplication.class, args);
